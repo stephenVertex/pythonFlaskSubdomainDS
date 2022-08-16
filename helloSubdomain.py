ace51124dee5712b072b6fee3dcb170af0a8983e
Stephen Barr (Vertex Development) <stephen.barr@devfactory.com>
2022-08-16 19:55:24 +0000

    Adding code to show the request URL

--- helloSubdomain.py
+++ helloSubdomain.py
@@ -1,15 +1,20 @@
 from flask import Flask
 import platform
+from flask import request
 app = Flask(__name__)
 
 
 @app.route("/", subdomain="<subdomain>")
 def hello_subdomain(subdomain):
+    print(request)
     x = "<h1><p>Hello, there!</p></h1><h2>Your subdomain:</h2> " + subdomain
     return x
 
 @app.route("/")
 def hello_regular():
-    x = "<h1><p>Hello, there!</p></h1><h2>No subdomain.</h2> " 
+    print(request)
+    print("MY URL: " + request.base_url)
+    my_url = request.base_url.replace("http://", "")
+    x = f"<h1><p>Hello, there!</p></h1><h2>{my_url}</h2> " 
     return x
     
